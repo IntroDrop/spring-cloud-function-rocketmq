194c92b8f72bb63e4f1d6b9d4e77e8ca81f3f7bf
IntroDrop <skyhigh0u@gmail.com>
2021-05-24 22:10:15 +0800

    a

---
+++ src/main/java/com/reverie/springcloudfunctionrocketmq/SpringCloudFunctionRocketmqApplication.java
@@ -0,0 +1,13 @@
+package com.reverie.springcloudfunctionrocketmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudFunctionRocketmqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudFunctionRocketmqApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/reverie/springcloudfunctionrocketmq/SpringCloudFunctionRocketmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reverie.springcloudfunctionrocketmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudFunctionRocketmqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
